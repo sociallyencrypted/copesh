a05a5f765340d7f4195fe24c51ca90ad032fce70
sociallyencrypted <mehularora810@gmail.com>
2022-10-02 20:16:05 +0530

    basic i/o and username

---
+++ Makefile
@@ -0,0 +1,4 @@
+copesh: src/copesh.c
+	gcc -o copesh src/copesh.c
+clean:
+	rm copesh
---
+++ src/copesh.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// global variables
+char *user;
+
+char *login()
+{
+    while (1)
+    {
+        char username[100];
+        printf("Username: ");
+        if (fgets(username, 100, stdin) == NULL)
+        {
+            printf("Error: Username too long");
+        }
+        else
+        {
+            strtok(username, "\n");
+            return username;
+        }
+    }
+}
+
+int main()
+{
+    printf("copesh v1.0.0\n");
+    user = login();
+    while (1)
+    {
+        printf("copesh:~ %s$ ", user);
+        char input[100];
+        scanf("%s", input);
+        if (strcmp(input, "exit") == 0)
+        {
+            printf("\n");
+            return 0;
+        }
+        else
+        {
+            printf("In development\n");
+        }
+    }
+}
