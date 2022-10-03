196310abb4e36187cef27904b45ae36451cd7e52
sociallyencrypted <mehularora810@gmail.com>
2022-10-04 00:26:10 +0530

    added arguments to date

--- src/date.c
+++ src/date.c
@@ -2,14 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    int flag_r = 0;
+    int flag_u = 0;
+    int timeFromEpoch = 0;
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "-ru") == 0 || strcmp(argv[i], "-ur") == 0)
+            {
+                flag_r = 1;
+                if (i + 1 < argc)
+                {
+                    timeFromEpoch = atoi(argv[i + 1]);
+                }
+                else
+                {
+                    printf("date: 'r' needs an integer argument.\n");
+                    printf("Try 'date --help' for more information.\n");
+                    exit(1);
+                }
+            }
+            if (strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "-ur") == 0 || strcmp(argv[i], "-ru") == 0)
+            {
+                flag_u = 1;
+            }
+        }
+    }
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     // Format: Mon Oct  3 11:28:53 IST 2022
+    int dayConsidered = tm.tm_mday;
+    int monthConsidered = tm.tm_mon;
+    if (flag_r == 1)
+    {
+        // time from epoch
+        t = timeFromEpoch;
+        tm = *localtime(&t);
+        dayConsidered = tm.tm_mday;
+        monthConsidered = tm.tm_mon;
+    }
+    if (flag_u == 1)
+    {
+        // convert time to UTC
+        tm = *gmtime(&t);
+        dayConsidered = tm.tm_mday;
+        monthConsidered = tm.tm_mon;
+    }
     char *day;
-    switch (tm.tm_wday)
+    char *month;
+    switch (dayConsidered)
     {
     case 0:
         day = "Sun";
@@ -33,8 +79,7 @@ int main()
         day = "Sat";
         break;
     }
-    char *month;
-    switch (tm.tm_mon)
+    switch (monthConsidered)
     {
     case 0:
         month = "Jan";
@@ -73,6 +118,13 @@ int main()
         month = "Dec";
         break;
     }
-    printf("%s %s %2d %02d:%02d:%02d %s %d\n", day, month, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_zone, tm.tm_year + 1900);
+    if (flag_u == 1)
+    {
+        printf("%s %s %2d %02d:%02d:%02d UTC %d\n", day, month, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_year + 1900);
+    }
+    else
+    {
+        printf("%s %s %2d %02d:%02d:%02d %s %d\n", day, month, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, tm.tm_zone, tm.tm_year + 1900);
+    }
     return 0;
 }
