be1501e51709460351ae6b3b758e695dceaae330
Mehul <mehularora810@gmail.com>
2022-10-14 15:03:06 +0530

    MKDIR FIXED

--- src/mkdir.c
+++ src/mkdir.c
@@ -9,6 +9,7 @@
 
 int main(int argc, char **argv)
 {
+    int directoryCount = 0;
     int flag_m = 0;
     int perms = 0777;
     int flag_p = 0;
@@ -17,18 +18,31 @@ int main(int argc, char **argv)
         printf("Usage: mkdir [-m mode] [-p] [args]\n");
         exit(1);
     }
+    if (argc==2){
+        if (strcmp(argv[1],"-p")==0 || strcmp(argv[1],"-m")==0 || strcmp(argv[1],"-mp")==0 || strcmp(argv[1],"-pm")==0){
+            printf("Usage: mkdir [-m mode] [-p] [args]\n");
+            exit(1);
+        }
+        else{
+            if (mkdir(argv[1],perms)==-1){
+                perror("mkdir");
+                exit(1);
+            }
+            exit(0);
+        }
+    }
     if (argc > 2)
     {
-        int directoryCount = 0;
         for (int i = 1; i < argc; i++)
         {
-            if (strncmp(argv[i], "-m", 2) == 0 || strncmp(argv[i], "-mp", 3) == 0 || strncmp(argv[i], "-pm", 3) == 0 || strncmp(argv[i], "--mode", 4))
+            if (strncmp(argv[i], "-m", 2) == 0 || strncmp(argv[i], "-mp", 3) == 0 || strncmp(argv[i], "-pm", 3) == 0 || strncmp(argv[i], "--mode", 6) == 0)
             {
                 flag_m = 1;
                 //--mode=perms or -mp=perms or -pm=perms
-                perms = strtok(argv[i], "=")[1];
+                char * sperms = strchr(argv[i], '=') + 1;
+                perms = strtol(sperms, NULL, 8);
                 //check if perms is a valid octal number
-                if (perms > 777 || perms < 0)
+                if (perms > 0777 || perms < 00)
                 {
                     printf("mkdir: invalid mode: ‘%s’\n", perms);
                     exit(1);
@@ -42,39 +56,48 @@ int main(int argc, char **argv)
             {
                 directoriesToBeCreated[directoryCount] = argv[i];
                 directoryCount++;
+                if (directoryCount==100){
+                    printf("mkdir: too many arguments\n");
+                    exit(1);
+                }
             }
         }
     }
     // make a directory given in argv
     char *currentDirectory = getcwd(NULL, 0);
-    char *dirName = directoriesToBeCreated[0];
-    char *path = (char *)malloc(strlen(currentDirectory) + strlen(dirName) + 2);
-    strcpy(path, currentDirectory);
-    strcat(path, "/");
-    strcat(path, dirName);
-    if (flag_p == 1)
-    {
-        // recursive mkdir
-        char *token = strtok(path, "/");
-        char *prev = (char *)malloc(1);
-        prev[0] = '\0';
-        while (token != NULL)
+    for (int i=0; i<directoryCount;i++){
+        if (directoriesToBeCreated[i]==NULL){
+            break;
+        }
+        char *dirName = directoriesToBeCreated[i];
+        if (flag_p == 1)
         {
-            char *newPath = (char *)malloc(strlen(prev) + strlen(token) + 2);
-            strcpy(newPath, prev);
-            strcat(newPath, "/");
-            strcat(newPath, token);
-            if (mkdir(newPath, perms) == -1)
+            //create all directories in the path
+            char* path = malloc(strlen(currentDirectory) + strlen(dirName) + 2);
+            strcpy(path, currentDirectory);
+            strcat(path, "/");
+            char *toBeAdded = strtok(dirName, "/");
+            while (toBeAdded != NULL)
+            {
+                strcat(path, toBeAdded);
+                if (mkdir(path, perms) == -1)
+                {
+                    printf("mkdir: cannot create directory ‘%s’: File exists\n", path);
+                    exit(1);
+                }
+                strcat(path, "/");
+                toBeAdded = strtok(NULL, "/");
+            }
+        }
+        else
+        {
+            printf("%s\n", dirName);
+            printf("%d\n", perms);
+            if (mkdir(dirName, perms) == -1)
             {
                 perror("mkdir");
                 exit(1);
             }
-            prev = newPath;
-            token = strtok(NULL, "/");
         }
-    }
-    else
-    {
-        mkdir(path, perms);
     }
 }
